2ee64c14e71512e9b542a6aab4a4303596e3842a
MaksGovor <maksgovruha@gmail.com>
2022-09-27 14:11:24 +0300

    Add keys

---
+++ GameLab2/Assets/Scripts/KeysCollect.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysCollect : MonoBehaviour
+{
+    public bool rotate = true; // do you want it to rotate?
+    public float rotationSpeed = 50;
+    public GameObject collectEffect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate)
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CollectKey();
+        }
+    }
+
+    public void CollectKey()
+    {
+        if (collectEffect)
+            Instantiate(collectEffect, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
